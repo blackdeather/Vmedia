7f2d244abfb1afdeff4cd1e87bf1c295cf7a7e53
blackdeather <blackdeather666@gmail.com>
2021-10-04 21:57:36 +0300

    add second window

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Главное окно')),
+        body: Center(child: RaisedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+        }, child: Text('Открыть второе окно'))),
+      ),
+    );
   }
 }
+
+class SecondScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('second page'),),
+        body: Column(
+          children: <Widget>[
+            TextField(onTap: (){}, style: TextStyle(fontSize: 24), maxLength: 30, maxLines: 1, textAlignVertical: TextAlignVertical.center,),
+            Center(child: RaisedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: Text('back'),
+            ),),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
