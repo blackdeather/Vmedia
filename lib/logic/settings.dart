dbea96f67fe9c50d15b9e6923b009ca3cecb18ca
blackdeather <blackdeather666@gmail.com>
2021-10-05 10:33:24 +0300

    add setting screen

---
+++ lib/logic/settings.dart
@@ -0,0 +1,7 @@
+class VmSettings{
+  String token = '';
+  int numberOfResult = 20;
+  int adult = 1; // adult filters 1 - off, 0 - on
+  int hdVideo = 1; // search for only HD video 1 - on, 0 - off
+  String filters = 'youtube';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text('Главное окно')),
         body: Center(child: RaisedButton(onPressed: (){
           Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+
         }, child: Text('Открыть второе окно'))),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => SettingScreen()));
+          },
+          child: Icon(Icons.settings),
+        ),
+
       ),
+
     );
   }
 }
@@ -31,6 +40,33 @@ class SecondScreen extends StatelessWidget {
         body: Column(
           children: <Widget>[
             TextField(onTap: (){}, style: TextStyle(fontSize: 24), maxLength: 30, maxLines: 1, textAlignVertical: TextAlignVertical.center,),
+
+            Center(child: RaisedButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              child: Text('back'),
+            ),),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class SettingScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('settings'),),
+        body: Column(
+          children: <Widget>[
+
+
             Center(child: RaisedButton(
               onPressed: (){
                 Navigator.pop(context);
